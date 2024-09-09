983a13f5cae7a449b191cc0381112e56995ef715
JoGyoungJun <yoop80075@gmail.com>
2024-09-09 14:51:58 +0900

    close #4

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 const Test2 =  ()=>{
-  return <div>
-    <input name="text" />
+  const [state, setState] = useState();
+  return (
+  <div>
+    <input name="text" onChange={(e)=>setState(e.target.value)}/>
+    <div>{state}</div>
   </div>
+  );
 }
 
 export default Test2
