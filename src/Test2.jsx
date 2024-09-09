2fd64601b59f90c543538598fc506c0ad66313e0
JoGyoungJun <yoop80075@gmail.com>
2024-09-09 15:06:12 +0900

    close #5

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -2,9 +2,13 @@ import { useState } from "react"
 
 const Test2 =  ()=>{
   const [state, setState] = useState();
+  const onChangeHander = (e) => {
+    setState(e.target.value)
+  }
+
   return (
   <div>
-    <input name="text" onChange={(e)=>setState(e.target.value)}/>
+    <input name="text" onChange={onChangeHander}/>
     <div>{state}</div>
   </div>
   );
